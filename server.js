3f01f521f1a2964d0c13f423ec2fbc0b4e56f735
RampoB <sarungdirman123@gmail.com>
2023-12-11 13:47:42 +0700

    ada yang salah bagian server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const FileUpload = require("express-fileupload");
 const cors = require("cors");
+const ProductRoutes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -8,7 +9,8 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(FileUpload());
-
+app.use(express.static());
+app.use(ProductRoutes);
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 });
